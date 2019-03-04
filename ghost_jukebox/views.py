8060c6665fe5ead88629f9c06a0a88e037c34c0f
Nolan Hawkins <nolanhhawkins@gmail.com>
2019-03-04 02:00:24 +0000

    Fix image saving

--- ghost_jukebox/views.py
+++ ghost_jukebox/views.py
@@ -1,9 +1,11 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request, redirect, url_for
 from io import BytesIO
 from time import sleep
 from picamera import PiCamera
 from PIL import Image
 import time
+
+from ghost_jukebox import app, auth
 
 # initialize the camera
 camera = PiCamera()
@@ -18,5 +20,7 @@ def hello_world():
 @auth.login_required
 def capture():
     filename = '{}.jpg'.format(time.time())
-    camera.capture('imgs/{}'.format(filename))
-    return send_from_directory('imgs', filename)
+    camera.capture('ghost_jukebox/static/pics/{}'.format(filename))
+    url = url_for('static', filename='pics/{}'.format(filename))
+    return redirect(url)
+
